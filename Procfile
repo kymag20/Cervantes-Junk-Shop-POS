b6ef516877a38b2371ba446e445e5cfa978e5bb7
kymag20 <kymag20@gmail.com>
2026-06-16 18:20:02 +0800

    hays

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn junkshop_pos.wsgi:application
+web: python manage.py migrate && python manage.py ensure_default_admin && gunicorn junkshop_pos.wsgi:application
--- junkshop_pos/wsgi.py
+++ junkshop_pos/wsgi.py
@@ -10,7 +10,14 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
+from django.db import DatabaseError
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'junkshop_pos.settings')
 
 application = get_wsgi_application()
+
+try:
+    call_command('ensure_default_admin', verbosity=0)
+except DatabaseError as exc:
+    print(f'Could not ensure default admin account: {exc}')
